2d1b1d474fe9a649b160ea4f43e3105e2e27c72c
Princeton Rose <decoyzpr@gmail.com>
2019-03-03 01:22:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,14 @@
 def oxford_comma(array)
   phrase = ''
+  if array.size < 2
+    phrase += array[0]
+    return phrase
+  elsif array.size == 2
+    phrase += "#{word} and #{word}"
+    return phrase
+  end
   for word in array
-    if word == array.last and array.size > 1
+    if word == array.last and array.size > 2
       phrase += ", and #{word}"
     else
       phrase += ", #{word}"
